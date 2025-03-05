062291b5652a5db944c6d3dfddaf9b635961be08
AlexLepauvre <alex.lepauvre@ae.mpg.de>
2025-03-05 16:49:00 +0100

    Create vRSA_demo_run_vRSA_simulation.m
    Demonstrates how to test the face validity of the vRSA pipeline

---
+++ eeg_example/vRSA_demo_run_vRSA_simulation.m
@@ -0,0 +1,104 @@
+%% vRSA Example Script: Simulation
+%
+% This scripts demonstrates how to test the face validity of the vRSA
+% pipeline with the selected priors. Data are simulated with known effects
+% (and dimensions matching the data to analyze) and the vRSA pipeline is
+% run. As the ground truth effects are known, we can test the capability of
+% the pipeline to retrieve the parameters.
+% 
+% Data for this example are from:
+%    Miller, K.J., Schalk, G., Hermes, D., Ojemann, J.G. and Rao, R.P., 2016. 
+%    Spontaneous decoding of the timing and content of human object perception 
+%    from cortical surface recordings reveals complementary information in the 
+%    event-related potential and broadband spectral change. 
+%    PLoS Computational Biology, 12(1), p.e1004660.
+%
+% Author:      Peter Zeidman, Alex Lepauvre
+% Date:        2025-02-14
+% Version:     1.0
+
+%% Settings
+subjects = {'S1','S2','S3','S4','S5','S6','S7', 'S8', 'S9','S10'};
+subject = 1;
+D = spm_eeg_load(sprintf('subjects/RmD_%s.mat',subjects{subject}));
+[nmodes,ntimes,nstimuli] = size(D(:,:,:));  
+
+%% RSA settings
+% Get binary vectors of each condition:
+is_human = double(contains(D.conditions,'Human'));
+is_animal = double(contains(D.conditions,'Animal'));
+is_face = double(contains(D.conditions,'Face'));
+is_body = double(contains(D.conditions,'Body'));
+is_natural = double(contains(D.conditions,'Natural'));
+is_manmade = double(contains(D.conditions,'ManMade'));
+
+% Define contrasts:
+c = zeros(nstimuli, 5);
+c(:, 1) = is_human + is_animal - (is_natural + is_manmade); % Animate vs. inanimate
+c(:, 2) = is_human - is_animal; % Human vs. animal
+c(:, 3) = is_face - is_body; % Body vs. face
+c(:, 4) = is_natural - is_manmade; % Natural vs. manmade
+c(:, 5) = c(:, 2) .* c(:, 3); % Interaction between body part and species
+
+% Add conditions names:
+cnames = {};
+cnames{1} = 'Animate - Inanimate';   
+cnames{2} = 'Human - Animal';   
+cnames{3} = 'Face - Body';   
+cnames{4} = 'Natural - Manmade';   
+cnames{5} = 'Species * Body';   
+
+% Create FIR with bins of 50ms:
+xBF = struct();  
+xBF.dt = 1 / D.fsample;  % Sampling rate
+xBF.name = 'Finite Impulse Response';
+xBF.length = size(D, 2) * xBF.dt;
+xBF.order  = 16;
+xBF = spm_get_bf(xBF);
+Xt = xBF.bf(1:size(D, 2), 2:end);
+
+% Simulate the data:
+CV = zeros(size(Xt, 2), size(c, 2)); % Controls which effects are on and which are off: time bin x contrast
+CV(4, 5) = 1; % Turn the first contrast on in 3r to 5th time window
+CV(6, 1) = 1; % Turn the second contrast on in the 6th to 8th time window
+s = 0.14;
+Y = spm_eeg_simulate_covariance(Xt, c, s, nmodes, length(subjects), CV);
+
+%% Run vRSA with priors estimated from automated prior selection procedure
+S = struct();
+S.Xt = Xt;
+S.con_c = mat2cell(c, size(c, 1), ones(1, size(c, 2)));
+S.con_c_names = cnames;
+S.pE = -8;
+S.pV = 16;
+nbases = size(Xt,2);
+RSAs = cell(length(subjects),nbases);
+for s = 1:length(subjects)
+    % Load data    
+    y =  reshape(Y{s}', [nmodes, size(Y{s}, 1)/(nstimuli), nstimuli]);
+    % RSA settings
+    settings = S;
+    % Specify and estimate
+    RSAs(s,:) = spm_eeg_rsa_specify(settings,y);    
+    RSAs(s,:) = spm_eeg_rsa_estimate(RSAs(s,:));
+end
+% Save the results:
+save('subjects/RSAs-sim.mat','RSAs','-v7.3');
+% Run PEB / BMC
+[PEB,F] = spm_eeg_rsa_peb(RSAs, params=[1, 2, 3, 4, 5], FIR_bf=true, t=D.time');
+
+% Save the results
+save('subjects/PEB-sim.mat','F','PEB');
+
+% Take the sum of evidence for the whole model in each subject:
+F0 = sum(cellfun(@(x) x.F0, RSAs), 2);
+% Plot the results
+figure;
+bar(F0, 'k')
+xlabel('Subjects')
+ylabel('Log BF')
+title('Simulation evidence for each subject')
+set(gca, 'FontSize',12);
+figHandles = findobj('Type', 'figure'); % Find all open figures
+saveas(figHandles(1), 'Figure_5-3.svg'); % Save as svg
+fprintf('\n Simulation evidence range between %0.2f and %0.2f across subjects',  min(F0), max(F0))
