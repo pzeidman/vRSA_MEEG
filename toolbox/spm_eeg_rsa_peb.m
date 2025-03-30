ad341091f06dff7722a82ae722064e2e8d1df7ff
Alex lepauvre <alex.lepauvre@ae.mpg.de>
2025-03-30 19:43:03 +0200

    Adjust model comparison following modification to spm_eeg_rsa_specify
    Previously, the model was specified separate RSA structures for each basis function.  This was replaced by implementing a full covariance component matrix of dimension [n conditions x nXt] x [n conditions x nXt]. This requires different parsing of the output for plotting. In addition, I added a model comparison switching off all components associated with a particular contrast or basis function at once, rather than relying on summing the evidence of single components effects.

--- toolbox/spm_eeg_rsa_peb.m
+++ toolbox/spm_eeg_rsa_peb.m
@@ -49,13 +49,19 @@ end
 
 % Handle empty input
 if isempty(options.t)
-    options.t = 1:RSAs{1, 1}.M.ntimes;
+    options.t = 1:RSAs{1}.M.ntimes;
 end
 if ~isfield(RSAs{1, 1}, 'cnames') | isempty(RSAs{1, 1}.cnames)
     ncomp = length(RSAs{1, 1}.M.Q);
     RSAs{1, 1}.cnames = arrayfun(@(x) sprintf('cond-%d', x), 1:ncomp, ...
         'UniformOutput', false);
 end
+
+% Extract info about basis function and contrasts:
+nXt = RSAs{1}.M.nXt;
+qbf = RSAs{1}.qbf;
+ncon = length(RSAs{1}.con);
+qcon = RSAs{1}.qcon;
 
 %--------------------------------------------------------------------------
 % (1) Run a PEB model for each RSA
@@ -66,34 +72,18 @@ PEB  = spm_dcm_peb(RSAs, M, options.params);
 %--------------------------------------------------------------------------
 % (2) Compare PEB models with / without each parameter
 %--------------------------------------------------------------------------
-F = [];
-for i = 1:length(PEB)
-    % conditions x basis functions
-    F(:,i) = compare_models(PEB(i));
-end
-
-%--------------------------------------------------------------------------
-% (3) Accumulate evidence over basis functions & contrasts
-%--------------------------------------------------------------------------
-F_bfs       = sum(F,1);   % evidence per BF (summed across conditions)
-F_contrasts = sum(F,2);   % evidence per contrast/condition (summed across BFs)
-
-Pp_bfs = spm_softmax([F_bfs(:)'; zeros(1,length(F_bfs))]);
-Pp_bfs = Pp_bfs(1,:);
-
-Pp_contrasts = spm_softmax([F_contrasts(:)'; zeros(1,length(F_contrasts))]);
-Pp_contrasts = Pp_contrasts(1,:);
+[F, Pp, F_bfs, Pp_bfs, F_contrasts, Pp_contrasts] = compare_models(PEB, qbf, qcon);
 
 %--------------------------------------------------------------------------
 % (4) Gather second-order parameters (BB) per basis function
 %--------------------------------------------------------------------------
 BB_per_bf = {};
 for i = 1:size(RSAs,1)
-    for j = 1:size(RSAs,2)
+    for j = 1:nXt
         if i == 1
-            BB_per_bf{j} = RSAs{i,j}.BB;
+            BB_per_bf{j} = RSAs{i}.BB(j:nXt:end, j:nXt:end);
         else
-            BB_per_bf{j} = RSAs{i,j}.BB + RSAs{i,j}.BB;
+            BB_per_bf{j} = BB_per_bf{j} + RSAs{i}.BB(j:nXt:end, j:nXt:end);
         end
     end
 end
@@ -107,11 +97,11 @@ G  = [];
 for i = 1:size(RSAs,1)
     for j = included_bfs
         if i == 1
-            BB = RSAs{i,j}.BB;
-            G  = RSAs{i,j}.G;
+            BB = RSAs{i}.BB(j:nXt:end, j:nXt:end);
+            G  = RSAs{i}.G(j:nXt:end, j:nXt:end);
         else
-            BB = BB + RSAs{i,j}.BB;
-            G  = G + RSAs{i,j}.G;
+            BB = BB + RSAs{i}.BB(j:nXt:end, j:nXt:end);
+            G  = G + RSAs{i}.G(j:nXt:end, j:nXt:end);
         end
     end
 end
@@ -119,9 +109,8 @@ end
 %--------------------------------------------------------------------------
 % (6) Extract parameters posterior expectation (Ep) and variance (Cp)
 %--------------------------------------------------------------------------
-Ep = full(horzcat(PEB.Ep));
-Cp = cell2mat(arrayfun(@(x) diag(full(x.Cp)), PEB, ...
-    'UniformOutput', false));
+Ep = reshape(full(PEB.Ep), ncon, nXt);
+Cp = PEB.Cp;
 
 %--------------------------------------------------------------------------
 % (6) Stop if no plotting is requested
@@ -137,11 +126,12 @@ end
 %--------------------------------------------------------------------------
 % Extract basic info
 %--------------------------------------------------------------------------
-nXt    = RSAs{1, 1}.M.nXt;    % # basis functions
-Xt     = RSAs{1, 1}.M.Xt;     % First-order basis design
-Xt_names = RSAs{1, 1}.M.Xt_names;     % First-order basis design
-Qnames = RSAs{1, 1}.cnames;   % Names of components
-nQ     = size(Qnames, 2);
+Xt        = RSAs{1, 1}.M.Xt;       % First-order basis functions
+Xt_names  = RSAs{1, 1}.M.Xt_names; % First-order basis functions names
+con       = RSAs{1, 1}.con;        % Contrasts
+cnames    = RSAs{1, 1}.cnames;     % Contrasts names
+Qnames    = RSAs{1, 1}.qnames;     % Name of the components
+nQ        = size(Qnames, 2);       % Number of components
 
 %--------------------------------------------------------------------------
 % FIGURE 1: First and second order design
@@ -174,7 +164,7 @@ end
 %------------------------------------
 % (2) MIDDLE+BOTTOM: Second-order matrices
 %------------------------------------
-nGrid = ceil(sqrt(nQ));  % dimension of grid for Q-matrices
+nGrid = ceil(sqrt(ncon));  % dimension of grid for Q-matrices
 
 % Nested layout: tile #2, spanning two rows
 layout2 = tiledlayout(mainLayout, nGrid, nGrid, 'TileSpacing','compact', 'Padding','compact');
@@ -182,12 +172,12 @@ layout2.Layout.Tile     = 2;
 layout2.Layout.TileSpan = [2 1];
 title(layout2, 'Model matrices');
 
-for q = 1:nQ
+for c = 1:ncon
     nexttile(layout2);
-    imagesc(RSAs{1}.M.Q{q});
+    imagesc(con{c} * con{c}');
     colormap gray;
     axis square;
-    title(Qnames{q});
+    title(cnames{c});
     set(gca, 'FontSize',12);
 end
 
@@ -207,12 +197,12 @@ if options.FIR_bf
     % (A) Plot the estimated parameters:
     nexttile(mainLayout)
     ctr = 1;
-    for q = 1:nQ-1
+    for c = 1:ncon
         hold on
-        spm_plot_ci(Ep(q,:), Cp(q,:), t);
+        spm_plot_ci(Ep(c,:), diag(Cp(c == qcon,c == qcon)), t);
         lbl{ctr}   = '';
-        lbl{ctr+1} = Qnames{q};
-        ctr        = ctr + 2; 
+        lbl{ctr+1} = cnames{c};
+        ctr        = ctr + 2;
     end
     title('Estimated hyperparameters');
     ylabel('v');
@@ -220,12 +210,12 @@ if options.FIR_bf
     xlim([t(1), t(end)]);
     legend(lbl, 'Location','best');
     set(gca, 'FontSize',12);
-    
+
     % (B) Plot the exponentiated estimated parameters:
     nexttile(mainLayout)
-    for q = 1:nQ-1
+    for c = 1:ncon
         hold on
-        spm_plot_ci(Ep(q,:), Cp(q,:), t, [], 'exp');
+        spm_plot_ci(Ep(c,:), diag(Cp(c == qcon,c == qcon)), t, [], 'exp');
     end
     title('Estimated hyperparameters');
     ylabel('Weights (exp)');
@@ -237,12 +227,12 @@ if options.FIR_bf
     % (C) Plot log evidence per basis function
     nexttile(mainLayout)
     h = plot(t, F, '-o');
-    set(h, {'MarkerFaceColor'}, get(h,'Color')); 
+    set(h, {'MarkerFaceColor'}, get(h,'Color'));
     title('Log evidence per basis function');
     ylabel('log BF');
     xlabel('Time');
     xlim([t(1), t(end)]);
-    legend(RSAs{1}.cnames, 'Location','best');
+    legend(cnames, 'Location','best');
     set(gca, 'FontSize',12);
 
     % (B) Covariance matrices at each time point, etc.
@@ -252,46 +242,53 @@ if options.FIR_bf
     title(mainLayout, 'Covariance matrices');
     ax_logbf = [];
     ctr = 1;
-    for i = 1:(size(RSAs,2))
+    for bf = 1:nXt
         nexttile(mainLayout)
-        imagesc(BB_per_bf{i})
+        imagesc(BB_per_bf{bf})
         colormap gray;
         set(gca,'YTickLabel',[], 'XTickLabel',[]);
         title(sprintf("%.2f-%.2f s", t(i) - bin_dur / 2, t(i) + bin_dur / 2))
         axis square;
 
         ax_logbf(ctr) = nexttile(mainLayout);
-        bar(F(:, i), 'FaceColor', [0.3, 0.3, 0.3])
+        bar(F(:, bf), 'FaceColor', [0.3, 0.3, 0.3])
         hold on
         yline(3, 'Color', 'r', 'LineStyle', '--')
         set(gca,'XTickLabel',[]);
         ctr = ctr + 1;
     end
-    set(gca,'XTickLabel',Qnames);
+    set(gca,'XTickLabel', cnames);
     ylabel('LogBF')
     set(gca,'FontSize',9);
     linkaxes(ax_logbf)
 
 else
-    fig = figure('Name', 'Group evidence BF', 'Position', [10, 10, 725, 725], 'Color',[1 1 1]);
-    tiledlayout(size(RSAs,2), 2, 'TileSpacing','compact','Padding','compact');
-    for i = 1:(size(RSAs,2))
+    figure('Name', 'Beta covariance per BF', 'Position', [10, 10, 725, 725], 'Color',[1 1 1]);
+    nGrid = ceil(sqrt(nXt));  % dimension of grid for Q-matrices
+    tiledlayout(nGrid, nGrid, 'TileSpacing','compact','Padding','compact');
+    for bf = 1:nXt
         nexttile()
-        imagesc(BB_per_bf{i})
+        imagesc(BB_per_bf{bf})
         colormap gray;
         axis square;
         xlabel("Stimuli")
         ylabel("Stimuli")
         set(gca,'FontSize',9);
-        title(sprintf("Beta 2nd order (%s)", Xt_names{i}), 'FontSize', 12)
-
+        title(sprintf("Beta 2nd order (%s)", Xt_names{bf}), 'FontSize', 12)
+    end
+    figure('Name', 'Group evidence BF', 'Position', [10, 10, 725, 725], 'Color',[1 1 1]);
+    nGrid = ceil(sqrt(nXt));  % dimension of grid for Q-matrices
+    tiledlayout(nGrid, nGrid, 'TileSpacing','compact','Padding','compact');
+    for bf = 1:nXt
         nexttile()
-        bar(F(:, i), 'FaceColor', [0.3, 0.3, 0.3])
+        bar(F(:, bf), 'FaceColor', [0.3, 0.3, 0.3])
         axis square;
         ylabel('Log evidence (nats)')
         set(gca,'FontSize',9);
-        set(gca,'XTickLabel',Qnames);
-        title(sprintf("evidence %s", Xt_names{i}), 'FontSize', 12)
+        title(sprintf("evidence %s", Xt_names{bf}), 'FontSize', 12)
+        if bf == nXt
+            set(gca,'XTickLabel',cnames);
+        end
     end
 end
 
@@ -311,7 +308,7 @@ title('Pooled evidence per component','FontSize',16);
 
 nexttile(mainLayout)
 bar(Pp_contrasts, 'FaceColor', [0.3, 0.3, 0.3]);
-set(gca,'XTick',1:nQ,'XTickLabel',RSAs{1}.cnames);
+set(gca,'XTick',1:nQ,'XTickLabel',cnames);
 ylabel('Probability');
 set(gca,'FontSize',12);
 title('Probability per component','FontSize',16);
@@ -343,14 +340,14 @@ else
     bar(F_bfs, 'FaceColor', [0.3, 0.3, 0.3]);
     ylabel('Log evidence (nats)');
     xlabel('Basis function');
-    xticklabels(Xt_names)
+    xticks(1:nXt); xticklabels(1:nXt)
     set(gca,'FontSize',12);
     title('Pooled evidence per BF','FontSize',16);
 
     nexttile(mainLayout)
     bar(Pp_bfs, 'FaceColor', [0.3, 0.3, 0.3]);
     ylabel('Probability');
-    xticklabels(Xt_names)
+    xticks(1:nXt); xticklabels(1:nXt)
     xlabel('Basis function');
     set(gca,'FontSize',12);
     title('Probability per BF','FontSize',16);
@@ -375,28 +372,68 @@ axis square;
 %==========================================================================
 % SUBFUNCTION: compare_models
 %==========================================================================
-function [F,P] = compare_models(PEB)
-% Compare each parameter switched off vs. on in a PEB model
-% and produce log BF values for each parameter.
+function [F, Pp, F_bfs, Pp_bfs, F_contrasts, Pp_contrasts] = compare_models(PEB, qbf, qcon)
+% Compute evidence for each component by switching them off sequentially
+% and by switching groups of parameters (for each contrast and each basis
+% function separately).
 
+%------------------------------------------
+% (1) Extract numbers of parameters
+%------------------------------------------
 n = length(PEB.Pnames);
+n_contrasts = length(unique(qcon(~isnan(qcon))));
+n_bf = length(unique(qbf(~isnan(qbf))));
 
+%------------------------------------------
+% (2) Compute evidence for each parameter
+%------------------------------------------
 F = zeros(1,n);
 for i = 1:n
-    F(i) = reduce_parameters(PEB,i);
+    msk = zeros(n, 1);
+    msk(i) = 1;
+    F(i) = reduce_parameters(PEB,msk==1);
 end
-
 % Flip log BF to evidence in favour of the full model
 F = -F;
-
 % Compute posterior probability that each parameter is needed
-P = spm_softmax([F; zeros(1,n)]);
-P = P(1,:);
+Pp = spm_softmax([F; zeros(1,n)]);
+Pp = reshape(Pp(1,:), n_contrasts, n_bf); % n contrasts x n bf
+F = reshape(F, n_contrasts, n_bf);        % n contrasts x n bf
+
+%------------------------------------------
+% (3) Compute evidence for each basis function
+%------------------------------------------
+F_bfs = zeros(1,n_bf);
+for i = 1:n_bf
+    msk = zeros(n, 1);
+    msk(qbf == i) = 1;
+    F_bfs(i) = reduce_parameters(PEB,msk==1);
+end
+% Flip log BF to evidence in favour of the full model
+F_bfs = -F_bfs;
+% Compute posterior probability that each parameter is needed
+Pp_bfs = spm_softmax([F_bfs; zeros(1,n_bf)]);
+Pp_bfs = Pp_bfs(1, :);
+
+%------------------------------------------
+% (4) Compute evidence for each contrast
+%------------------------------------------
+F_contrasts = zeros(1,n_contrasts);
+for i = 1:n_contrasts
+    msk = zeros(n, 1);
+    msk(qcon == i) = 1;
+    F_contrasts(i) = reduce_parameters(PEB,msk==1);
+end
+% Flip log BF to evidence in favour of the full model
+F_contrasts = -F_contrasts;
+% Compute posterior probability that each parameter is needed
+Pp_contrasts = spm_softmax([F_contrasts; zeros(1,n_contrasts)]);
+Pp_contrasts = Pp_contrasts(1, :);
 
 %==========================================================================
 % SUBFUNCTION: reduce_parameters
 %==========================================================================
-function logBF = reduce_parameters(PEB,q)
+function logBF = reduce_parameters(PEB,msk)
 % Switch off the q-th parameter in the PEB model, then compute the log Bayes
 % factor in favour of that reduced model (versus the full one).
 
@@ -407,7 +444,7 @@ pC = PEB.M.pC;
 
 % Remove prior variance from parameter q
 rC = pC;
-rC(q,q) = 0;
+rC(msk,msk) = 0;
 
 % Evaluate difference in log evidence for the reduced model
 logBF = spm_log_evidence_reduce(Ep, Cp, pE, pC, pE, rC);
